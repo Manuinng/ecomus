fb287d5012569a0751a4fb688f517ac6e6d6dc19
Manuinng <manuel.quijadapo@gmail.com>
2025-05-22 15:45:40 -0400

    Remove redundant instructions from Ecomus report generation script

--- Ecomus Report/report.py
+++ Ecomus Report/report.py
@@ -1,7 +1,3 @@
 """Beginner's Guide to Ecomus Report Generation"""
 print("Welcome to the Ecomus Report Generation Guide!")
 print("This script will help you generate a report for your Ecomus project.")
-print("Please follow the instructions carefully.")
-print("1. Ensure you have all the necessary data files in the 'data' directory.")
-print("2. Make sure you have the required libraries installed.")
-print("3. Run the script to generate the report.")
