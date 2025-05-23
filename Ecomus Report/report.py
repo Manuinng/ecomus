7742bba9d5fc680b2c20ac7f06c87b9f1672909c
Manuinng <manuel.quijadapo@gmail.com>
2025-05-23 14:41:49 -0400

    Add additional instruction message to Ecomus report generation script

--- Ecomus Report/report.py
+++ Ecomus Report/report.py
@@ -1,3 +1,4 @@
 """Beginner's Guide to Ecomus Report Generation"""
 print("Welcome to the Ecomus Report Generation Guide!")
 print("This script will help you generate a report for your Ecomus project.")
+print("Please follow the instructions carefully.")
