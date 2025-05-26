9723aaba53c9e837efbaf7e36164b0e3fb4585b2
Manuinng <manuel.quijadapo@gmail.com>
2025-05-25 22:02:30 -0400

    Add additional instructions for Ecomus report generation

--- Ecomus Report/report.py
+++ Ecomus Report/report.py
@@ -2,3 +2,4 @@
 print("Welcome to the Ecomus Report Generation Guide!")
 print("This script will help you generate a report for your Ecomus project.")
 print("Please follow the instructions carefully.")
+print("Make sure you have all the necessary data ready before proceeding.")
